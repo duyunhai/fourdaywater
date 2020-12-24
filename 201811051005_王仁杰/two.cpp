87d733306e9a215f4f4d2d38058ba57a108a82a1
wrj163766 <16376678182qq.com>
2020-12-24 09:16:08 +0800

    两数合并(引用参数)

---
+++ 201811051005_王仁杰/two.cpp
@@ -0,0 +1,17 @@
+#include"iostream"
+using namespace std;
+
+void fun(int &a,int &b)
+{
+	int c;
+    c=b/10+a/10*10+b%10*100+a%10*1000;
+    cout<<"The new number after synthesis is:"<<c<<endl;
+
+}
+void main()
+{
+	int m,n;
+	cout<<"Please input two integer numbers:";
+    cin>>m>>n;
+	fun(m,n);
+}
