bff9460d4039e4d876120b4d683897e9ab54b342
GD-W-2020 <wgd_2018@163.com>
2020-12-25 09:53:37 +0800

    猴子选大王（数组）

---
+++ 201811050994/猴子选大王_数组.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n=0 , num,i,x;//n的初始值为0
+	int a[100], m; //猴子的数量小于100
+	n < m;
+	cout << "请输入m:" << endl;
+	cin >> m;
+	cout << "请输入x:" << endl;
+	cin >> x;
+	num = m;//记录未退出的猴子数
+	for (i = 1; i <= m; i++)
+	{
+		a[i] = 1;//每个猴子标记为1，表示该猴子未退出
+	}
+	while (num != 1)//当只有一猴子时，退出循环
+	{
+		for (i = 1; i <= m; i++)
+		{
+			if (a[i] == 1)
+			{
+				n++;
+			}
+			if (n==x)
+			{
+				a[i] = 0; 
+				num--;   //退出一只猴子
+				n= 0;
+			}
+		}
+	}
+	for (i = 1; i <= m; i++)
+	{
+		if (a[i] == 1)//输出最后剩下的那只猴子，猴王
+		{
+			cout <<"i="<<i<< endl;
+			break;
+		}
+	}
+	return 0;
+}
